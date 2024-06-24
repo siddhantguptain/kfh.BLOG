0313e6abd7ea66bdb324f9bc424c4fb755fb3bfe
Siddhant Gupta <me@siddhantgupta.in>
2024-06-24 18:53:31 +0530

    Added the Container and Wrapper for responsiveness and common CSS styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="mainContainer">
+            <div className="mainWrfapper">
+                {children}
+            </div>   
+        </div>
+      </body>
     </html>
   );
 }
