3b4078da51cde44191a4fe8c67ce0a906e3eba60
Siddhant Gupta <me@siddhantgupta.in>
2024-07-11 13:43:05 +0530

    Updated the Category List

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
 import style from './CategoryList.module.css';
+import Image from "next/image";
+import Link from 'next/link';
+
 const CategoryList = () => {
   return (
-    <div>CategoryList</div>
+    <div className={style.container}>
+      <h1 className={style.title}>Popular Category</h1>
+      <div className={style.categories}>
+         <Link href="/blog?cat=style" className={`${style.category} ${style.style}`}>
+          <Image 
+            className={style.image}
+            src='/style.png'
+            width={32}
+            height={32}
+          /> 
+          Style
+          </Link>
+          <Link href="/blog?cat=fashion" className={`${style.category} ${style.fashion}`}>
+          <Image 
+            className={style.image}
+            src='/style.png'
+            width={32}
+            height={32}
+          /> 
+          Fashion
+          </Link>
+          <Link href="/blog?cat=food" className={`${style.category} ${style.food}`}>
+          <Image 
+            className={style.image}
+            src='/style.png'
+            width={32}
+            height={32}
+          /> 
+          Food
+          </Link>
+          <Link href="/blog?cat=travel" className={`${style.category} ${style.travel}`}>
+          <Image 
+            className={style.image}
+            src='/style.png'
+            width={32}
+            height={32}
+          /> 
+          travel
+          </Link>
+          <Link href="/blog?cat=culture" className={`${style.category} ${style.culture}`}>
+          <Image 
+            className={style.image}
+            src='/style.png'
+            width={32}
+            height={32}
+          /> 
+          culture
+          </Link>
+          <Link href="/blog?cat=coding" className={`${style.category} ${style.coding}`}>
+          <Image 
+            className={style.image}
+            src='/style.png'
+            width={32}
+            height={32}
+          /> 
+          coding
+          </Link>
+      </div>
+    </div>
   )
 }
 
